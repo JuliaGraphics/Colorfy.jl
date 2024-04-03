9de4b5808a9e3d96ec6269ae0e8efb35f1ae35e1
Elias Carvalho <eliascarvdev@gmail.com>
2024-04-03 08:43:21 -0300

    Add Unitful.jl extension

---
+++ ext/ColorfyUnitfulExt.jl
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------------
+# Licensed under the MIT License. See LICENSE in the project root.
+# -----------------------------------------------------------------
+
+module ColorfyUnitfulExt
+
+using Colorfy
+using Colorfy: Values
+using Unitful: Quantity, ustrip
+
+function Colorfy.getcolors(colorfier::Colorfier{<:Values{Quantity}})
+  values = ustrip.(Colorfy.values(colorfier))
+  alphas = Colorfy.alphas(colorfier)
+  colorscheme = Colorfy.colorscheme(colorfier)
+  colorrange = Colorfy.colorrange(colorfier)
+  unitless = Colorfier(values; alphas, colorscheme, colorrange)
+  Colorfy.getcolors(unitless)
+end
+
+end
