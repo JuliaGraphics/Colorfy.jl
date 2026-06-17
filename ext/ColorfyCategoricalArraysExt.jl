2819e6a8eecd0fcbdecb44cc7ac3fdbf29abfae6
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-06-17 15:16:34 -0300

    Improve get calls with colorschemes (#48)

--- ext/ColorfyCategoricalArraysExt.jl
+++ ext/ColorfyCategoricalArraysExt.jl
@@ -10,8 +10,10 @@ using CategoricalArrays: levels, levelcode
 import Colorfy
 
 function Colorfy.repr(values::AbstractVector{<:CategoricalValue}, colorscheme, colorrange)
+  # not all levels may be present in the input values,
+  # so we need to get the number of levels from the type
   n = length(levels(values))
-  c = colorscheme[range(n > 1 ? 0 : 1, 1, length=n)]
+  c = get(colorscheme, 1:n, colorrange)
   c[map(levelcode, values)]
 end
 
